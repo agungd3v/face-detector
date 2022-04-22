2dd870c82cb66c2e9d3e9190911d4ce378aa00a1
agungd3v <agungd3v@gmail.com>
2022-04-22 16:00:02 +0700

    update & add sample image

--- js/app.js
+++ js/app.js
@@ -1,69 +1,77 @@
-let image = document.querySelector('img')
-let video = document.querySelector('video')
-let canvas = document.querySelector('canvas')
-let ctx = canvas.getContext('2d')
-let displaySize, model;
+let options, reference;
 
-let width = 300
-let height = 300
+let withBoxes = true
+let scoreThreshold = 0.5
+let inputSize = 224
 
-const startSteam = () => {
-  navigator.mediaDevices.getUserMedia({
-    video: {width, height},
-    audio : false
-  }).then((steam) => {video.srcObject = steam})
+function resizeCanvasAndResults(dimensions, canvas, results) {
+  const { width, height } = dimensions instanceof HTMLVideoElement
+    ? faceapi.getMediaDimensions(dimensions)
+    : dimensions
+  canvas.width = width
+  canvas.height = height
+
+  return faceapi.resizeResults(results, { width: 300, height: 300 })
 }
 
-const detectFace = async () => {
-  const prediction = await model.estimateFaces(video, false)
-  ctx.drawImage(video, 0, 0, width, height)
+function drawLandmarks(dimensions, canvas, results, withBoxes = true) {
+  const resizedResults = resizeCanvasAndResults(dimensions, canvas, results)
 
-  prediction.forEach(pre => {
-    // ctx.beginPath()
-    // ctx.lineWidth = 2
-    // ctx.strokeStyle = 'blue'
-    // ctx.rect(pre.topLeft[0], pre.topLeft[1], pre.bottomRight[0] - pre.topLeft[0], pre.bottomRight[1], pre.topLeft[1])
-    // ctx.stroke()
+  if (withBoxes) {
+    // faceapi.drawDetection(canvas, resizedResults.map(det => det.detection))
+  }
 
-    ctx.fillStyle = 'red'
-    pre.landmarks.forEach(landmark => {
-      ctx.fillRect(landmark[0], landmark[1], 5, 5)
-    })
+  const faceLandmarks = resizedResults.map(det => det.landmarks)
+  const drawLandmarksOptions = {
+    lineWidth: 2,
+    drawLines: true,
+    color: 'green'
+  }
+
+  // faceapi.drawLandmarks(canvas, faceLandmarks, drawLandmarksOptions)
+}
+
+async function onPlay() {
+  const overlay = document.querySelector('canvas')
+  const videoEl = document.querySelector('video')
+  if (videoEl.paused || videoEl.ended || !faceapi.nets.tinyFaceDetector.params)
+    return setTimeout(() => onPlay())
+
+  const result = await faceapi.detectSingleFace(videoEl, options).withFaceLandmarks().withFaceDescriptor()
+
+  if (result) {
+    const faceMatcher = new faceapi.FaceMatcher(result)
+    drawLandmarks(videoEl, overlay, [result], withBoxes)
+
+    if (reference) {
+      const bestMatch = faceMatcher.findBestMatch(reference.descriptor)
+      document.querySelector('#rss').textContent = bestMatch._distance
+    }
+  }
+
+  setTimeout(() => onPlay())
+}
+
+async function run() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: {width: 300, height: 300},
+    audio: false
   })
+  const videoEl = document.querySelector('video')
+  videoEl.srcObject = stream;
+
+  options = new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
+
+  const imgEle = document.querySelector('img')
+  reference = await faceapi.detectSingleFace(imgEle, options).withFaceLandmarks().withFaceDescriptor()
 }
 
-startSteam()
-
-video.addEventListener('loadeddata', async () => {
-  model = await blazeface.load()
-  setInterval(detectFace, 100)
-})
-
-// Promise.all([
-//   faceapi.nets.ageGenderNet.loadFromUri('models'),
-//   faceapi.nets.ssdMobilenetv1.loadFromUri('models'),
-//   faceapi.nets.tinyFaceDetector.loadFromUri('models'),
-//   faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-//   faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-//   faceapi.nets.faceExpressionNet.loadFromUri('models')
-// ]).then(startSteam)
-
-
-// async function detect() {
-//   const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
-
-//   ctx.clearRect(0,0, width, height)
-//   const resizedDetections = faceapi.resizeResults(detections, displaySize)
-//   faceapi.draw.drawDetections(canvas, resizedDetections)
-
-//   resizedDetections.forEach(result => {
-//     new faceapi.draw.DrawTextField ([], result.detection.box.bottomRight).draw(canvas)
-//   })
-// }
-
-// video.addEventListener('play', () => {
-//   displaySize = {width, height}
-//   faceapi.matchDimensions(canvas, displaySize)
-
-//   setInterval(detect, 2000)
-// })
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri('models'),
+  faceapi.loadTinyFaceDetectorModel('models'),
+  faceapi.loadFaceLandmarkModel('models'),
+  faceapi.loadFaceRecognitionModel('models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+]).then(run)
