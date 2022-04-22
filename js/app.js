83c588bfd7df88ee0614352825b9ff66c3b44056
agungd3v <agungd3v@gmail.com>
2022-04-22 08:10:17 +0700

    update using tensorflow.js

--- js/app.js
+++ js/app.js
@@ -1,57 +1,69 @@
-let video = document.getElementById("video")
-let canvas = document.body.appendChild(document.createElement("canvas"))
-let ctx = canvas.getContext("2d")
-let displaySize;
+let image = document.querySelector('img')
+let video = document.querySelector('video')
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
+let displaySize, model;
 
 let width = 300
 let height = 300
 
 const startSteam = () => {
-  console.log("START STEAM");
   navigator.mediaDevices.getUserMedia({
     video: {width, height},
     audio : false
   }).then((steam) => {video.srcObject = steam})
 }
 
-// console.log(faceapi.nets);
+const detectFace = async () => {
+  const prediction = await model.estimateFaces(video, false)
+  ctx.drawImage(video, 0, 0, width, height)
 
-console.log("START LOAD MODEL")
-Promise.all([
-  faceapi.nets.ageGenderNet.loadFromUri('models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('models'),
-  faceapi.nets.tinyFaceDetector.loadFromUri('models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('models')
-]).then(startSteam)
+  prediction.forEach(pre => {
+    // ctx.beginPath()
+    // ctx.lineWidth = 2
+    // ctx.strokeStyle = 'blue'
+    // ctx.rect(pre.topLeft[0], pre.topLeft[1], pre.bottomRight[0] - pre.topLeft[0], pre.bottomRight[1], pre.topLeft[1])
+    // ctx.stroke()
 
-
-async function detect() {
-  const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
-  //console.log(detections);
-  
-  ctx.clearRect(0,0, width, height)
-  const resizedDetections = faceapi.resizeResults(detections, displaySize)
-  faceapi.draw.drawDetections(canvas, resizedDetections)
-  faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-  faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-
-  // console.log(resizedDetections);
-  resizedDetections.forEach(result => {
-    const {age, gender, genderProbability} = result;
-    new faceapi.draw.DrawTextField ([
-      // `${Math.round(age,0)} Tahun`,
-      // `${gender} ${Math.round(genderProbability)}`
-    ],
-    result.detection.box.bottomRight
-    ).draw(canvas)
-  });
+    ctx.fillStyle = 'red'
+    pre.landmarks.forEach(landmark => {
+      ctx.fillRect(landmark[0], landmark[1], 5, 5)
+    })
+  })
 }
 
-video.addEventListener('play', ()=> {
-  displaySize = {width, height}
-  faceapi.matchDimensions(canvas, displaySize)
+startSteam()
 
-  setInterval(detect, 100)
+video.addEventListener('loadeddata', async () => {
+  model = await blazeface.load()
+  setInterval(detectFace, 100)
 })
+
+// Promise.all([
+//   faceapi.nets.ageGenderNet.loadFromUri('models'),
+//   faceapi.nets.ssdMobilenetv1.loadFromUri('models'),
+//   faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+//   faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+//   faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+//   faceapi.nets.faceExpressionNet.loadFromUri('models')
+// ]).then(startSteam)
+
+
+// async function detect() {
+//   const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+
+//   ctx.clearRect(0,0, width, height)
+//   const resizedDetections = faceapi.resizeResults(detections, displaySize)
+//   faceapi.draw.drawDetections(canvas, resizedDetections)
+
+//   resizedDetections.forEach(result => {
+//     new faceapi.draw.DrawTextField ([], result.detection.box.bottomRight).draw(canvas)
+//   })
+// }
+
+// video.addEventListener('play', () => {
+//   displaySize = {width, height}
+//   faceapi.matchDimensions(canvas, displaySize)
+
+//   setInterval(detect, 2000)
+// })
